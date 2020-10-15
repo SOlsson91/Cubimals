bd65e03705ad6e304fa8f5358f2fdcb24ef6a397
Sebastian Olsson <sebastian.olsson@futuregames.nu>
2020-10-15 12:00:34 +0200

    Created the functionallity to spawn the player at specific location and change what animal is active

---
+++ Assets/Scripts/SpawnPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/*
+ * Class to spawn the player Prefab at a specific location
+ */
+
+public class SpawnPlayer : MonoBehaviour
+{
+    [SerializeField] Player playerToSpawn;
+    [SerializeField] Transform spawnPosition;
+
+    void Start()
+    {
+        Instantiate(playerToSpawn, spawnPosition.position, Quaternion.identity);
+    }
+}
