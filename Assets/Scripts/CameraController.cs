29b344670bed5f38c5e6242dea7d447d1fc6977a
Sebastian Olsson <sebastian.olsson@futuregames.nu>
2020-10-16 11:49:20 +0200

    Added a camera controller to follow the player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,18 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+/*
+ * Following the player with a small smoothing/dampening on the camera
+ * Important to change project settings to run SpawnPlayer before CameraController
+ */
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Transform target;
+    Vector3 offset;
+    Vector3 cameraPosition;
+    public Vector3 smoothness = new Vector3(3, 1, 3);
+
     void Start()
     {
-        
+        try
+        {
+            target = GameObject.FindWithTag("Player").transform;
+        }
+        catch
+        {
+            Debug.LogWarning("[CameraController] Cannot find Player");
+            Debug.LogWarning("Have you changed the project settings to have SpawnPlayer before CameraController?");
+            target = null;
+            return;
+        }
+        offset = transform.position - target.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        // If Player cannot be found retur;
+        if (target == null)
+            return;
+
+        cameraPosition = target.position + offset;
+        Vector3 position = Vector3.zero;
+        position.x = Mathf.Lerp(transform.position.x, cameraPosition.x, smoothness.x * Time.fixedDeltaTime);
+        position.y = Mathf.Lerp(transform.position.y, cameraPosition.y, smoothness.y * Time.fixedDeltaTime);
+        position.z = Mathf.Lerp(transform.position.z, cameraPosition.z, smoothness.z * Time.fixedDeltaTime);
+        transform.position = position;
     }
 }
