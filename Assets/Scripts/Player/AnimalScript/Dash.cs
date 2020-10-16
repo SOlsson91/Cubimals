ab408ee41f61cb8d0dd71d2f9ffad9b0ff6eae59
Jonathan <jonathan.isaksson@futuregames.nu>
2020-10-16 17:27:56 +0200

    Changes have been made to the Cow dash to make it more adjustable. Started on the "Bash" functionality

---
+++ Assets/Scripts/Player/AnimalScript/Dash.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Dash : MonoBehaviour
+{
+    public float dashSpeed = 100;
+    public float dashTime = 1;
+    public float cooldownValue = 1;
+
+    private float dashNext = 0;
+    private float cooldownNext = 0;
+
+    private bool dashing;
+
+    private void Start()
+    {
+        dashing = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && Time.time > cooldownNext)
+        {
+            Debug.Log("Dash");
+
+            dashing = true;
+            dashNext = dashTime;
+
+            cooldownNext = Time.time + cooldownValue;
+        }
+        else if(dashNext <= 0 && dashing == true)
+        {
+            dashing = false;
+
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+        else if(dashing == true)
+        {
+            dashNext -= Time.deltaTime;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (dashing == true)
+        {
+            gameObject.GetComponent<Rigidbody>().velocity = gameObject.transform.forward * dashSpeed * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Pushable")
+        {
+            
+        }
+    }
+}
