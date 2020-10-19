c40ee9cc52d6abc2abb60ee2fc1bb9fa48a1a59a
Sebastian Olsson <sebastian.olsson@futuregames.nu>
2020-10-19 14:55:49 +0200

    G0-20, Simple timer for the player to be timed when running the levels

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+/*
+ * Simple Time class to be used to get the time it takes for a player to finish a level
+ */
+
+public class Timer : MonoBehaviour
+{
+    public bool startOnLoad = true;
+    float time;
+    bool isActive = false;
+
+    public bool IsActive
+    {
+        get { return isActive; }
+        set { isActive = value; }
+    }
+
+    void Start()
+    {
+        time = 0;
+        isActive = startOnLoad;
+    }
+
+    void Update()
+    {
+        if (isActive)
+        {
+            time += Time.deltaTime;
+        }
+    }
+    public void ResetTimer() { time = 0; }
+    public float GetTime() { return time; }
+}
