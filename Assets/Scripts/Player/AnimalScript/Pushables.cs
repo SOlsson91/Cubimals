6daa63d3e9914f2882c2bd1d2146f11d57f239d9
Jonathan <jonathan.isaksson@futuregames.nu>
2020-10-19 14:44:56 +0200

    The best solution still seems to be to just increase the mass. I made a script to make all pushables(layer) increase their mass

---
+++ Assets/Scripts/Player/AnimalScript/Pushables.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pushables : MonoBehaviour
+{
+    private void Awake()
+    {
+        foreach(GameObject push in GameObject.FindGameObjectsWithTag("Pushable"))
+        {
+            push.AddComponent<Rigidbody>();
+            push.GetComponent<Rigidbody>().mass = 100;
+            push.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        }
+    }
+}
