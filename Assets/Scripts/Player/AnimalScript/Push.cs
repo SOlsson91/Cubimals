a2ce6eacb609824014279d39675bcb4b0e0ecaaa
Jonathan <jonathan.isaksson@futuregames.nu>
2020-10-19 13:27:26 +0200

    Rearrenged and made a separate script for the Cow's ability

---
+++ Assets/Scripts/Player/AnimalScript/Push.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Push : MonoBehaviour
+{
+    Player player;
+
+    private void Start()
+    {
+        player = gameObject.GetComponent<Player>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Pushable")
+        {
+            collision.gameObject.transform.position += player.transform.forward.normalized;
+        }
+    }
+}
