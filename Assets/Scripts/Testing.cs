ffb64ec915cf41820e270c880b8435551d51edcf
Jonathan <jonathan.isaksson@futuregames.nu>
2020-10-20 16:48:13 +0200

    Changes made nothing new except added the Mouse Control Scheme

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Testing : MonoBehaviour
 {
+    public Button btn;
 
+    private void Start()
+    {
+        btn.onClick.AddListener(Onclick);
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,5 +21,10 @@ public class Testing : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("I hit " + collision.gameObject.tag);
+    }
+
+    public void Onclick()
+    {
+        Debug.Log("Input Recieved!");
     }
 }
