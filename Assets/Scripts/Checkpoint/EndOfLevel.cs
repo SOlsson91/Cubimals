48b0ff0647dc6703ad895f372cb7bf314da8c599
Sebastian Olsson <sebastian.olsson@futuregames.nu>
2020-10-22 15:51:38 +0200

    Added a simple area to put at the end of the level to load the next level

---
+++ Assets/Scripts/Checkpoint/EndOfLevel.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class EndOfLevel : MonoBehaviour
+{
+    [SerializeField] string nextLevel = string.Empty;
+    int playersInGoal = 0;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponentInParent<Player>().CompareTag("Player"))
+        {
+            playersInGoal++;
+
+            if (playersInGoal == 2)
+            {
+                if (nextLevel != string.Empty)
+                {
+                    Debug.Log("NEXT LEVEL");
+                    GameManager.Instance.LoadLevel(nextLevel);
+                }
+                else
+                {
+                    Debug.LogWarning("[EndOfLevel] What level do you want to load?");
+                    return;
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponentInParent<Player>().CompareTag("Player"))
+        {
+            playersInGoal--;
+        }
+    }
+
+}
