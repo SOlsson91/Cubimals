f8eabac3885fea281b7a3cd43e1a40bf3b7f9cb5
Sebastian Olsson <sebastian.olsson@futuregames.nu>
2020-10-22 10:56:27 +0200

    Added so that the players do not spawn ontop of eachother G0-17

---
+++ Assets/Scripts/DeadCollider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DeadCollider : MonoBehaviour
+{
+    public Transform respawnPoint;
+    public Vector3 spawnOffset = new Vector3(0.0f, 1.25f, 0.0f);
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameManager.Instance.players.ForEach(delegate(Player player) {
+                int playerNumber = other.gameObject.GetComponent<Player>().playerNumber;
+                if (player.playerNumber == playerNumber)
+                {
+                    other.transform.position = respawnPoint.position + (spawnOffset * playerNumber);
+                }
+            });
+        }
+    }
+}
