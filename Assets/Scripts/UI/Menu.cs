389e441b0e88b0a35029d7412b8f1800ca9801e9
Sebastian Olsson <sebastian.olsson@futuregames.nu>
2020-10-26 10:10:00 +0100

    Updated the game so now it needs boot to run the game

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -1,9 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
+    SpawnPlayer spawner;
+    [Range(1,2), SerializeField] int playersToSpawn = 2;
+
+    void Start()
+    {
+        spawner = GetComponent<SpawnPlayer>();
+    }
+
+    public void StartGame(string sceneName)
+    {
+        for (int i = 0; i < playersToSpawn; ++i)
+        {
+            spawner.Spawn(i);
+            Debug.Log("Spawned");
+        }
+        GameManager.Instance.LoadLevel(sceneName);
+    }
+
     public void LoadScene(string sceneName)
     {
         GameManager.Instance.LoadLevel(sceneName);
