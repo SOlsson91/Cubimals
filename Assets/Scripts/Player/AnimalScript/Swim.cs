063f531d0b062735b873dc373ff9d50b509df765
Jonathan Johansson <jojo.jonathan@hotmail.com>
2020-10-27 09:11:44 +0100

    small fixes to camera position and paddin value

--- Assets/Scripts/Player/AnimalScript/Swim.cs
+++ Assets/Scripts/Player/AnimalScript/Swim.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Swim : Ability
 {
+    Rigidbody playerRigidbody;
+    Animal animal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animal = GetComponent<Animal>();
+        playerRigidbody = GetComponentInParent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Water") 
+        {
+            animal.movementSpeed = 10;
+        }
         
     }
 }
