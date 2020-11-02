c2609a010950e5a7c7c73b0c41d1d22526fda2d2
Sebastian Olsson <sebastian.olsson@futuregames.nu>
2020-11-02 11:35:29 +0100

    Removed some debug log, also updated a debug message

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -17,7 +17,7 @@ public class Menu : MonoBehaviour
         for (int i = 0; i < playersToSpawn; ++i)
         {
             spawner.Spawn(i);
-            Debug.Log("Spawned");
+            Debug.Log("[Menu] Spawned player " + i );
         }
         GameManager.Instance.ResetLives();
         GameManager.Instance.LoadLevel(sceneName);
@@ -27,18 +27,15 @@ public class Menu : MonoBehaviour
     public void LoadScene(string sceneName)
     {
         GameManager.Instance.LoadLevel(sceneName);
-        Debug.Log("Start Game input Recieved");
     }
 
     public void UnloadScene(string sceneName)
     {
         GameManager.Instance.UnloadLevel(sceneName);
-        Debug.Log("Unload input Recieved");
     }
 
     public void QuitGame()
     {
         Application.Quit();
-        Debug.Log("Quit input Recieved");
     }
 }
